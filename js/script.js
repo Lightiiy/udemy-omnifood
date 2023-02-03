08382a5eef232143bafc4ecae3728a615cadb969
Gabriel Bekier <gabrielbekier@gmail.com>
2023-02-03 21:45:14 +0100

    Navbar-functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log('JS conneceted');
+
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+const btnNavEl = document.querySelectorAll('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+btnNavEl.forEach(btnNavEl => {
+  btnNavEl.addEventListener(
+    'click',
+    (OpenNavMenu = () => {
+      headerEl.classList.toggle('nav-open');
+    })
+  );
+});
